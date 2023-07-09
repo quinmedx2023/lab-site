dc7177368ebbe674a2493dccb61d6ee660964ad0
quin <quineyugame@gmail.com>
2023-07-09 13:43:06 +0800

    add data.js

---
+++ src/data/data.js
@@ -0,0 +1,66 @@
+export const researches = [
+    {category:"Technologies", image:"https://bit.bme.jhu.edu/img/menu/menu_oct.jpg", url:"", abbreviations:"research 1"},
+    {category:"Technologies", image:"https://bit.bme.jhu.edu/img/menu/menu_nonlinear_endo.jpg", url:"", abbreviations:"research 2"},
+    {category:"Technologies", image:"https://bit.bme.jhu.edu/img/menu/menu_multimodal.jpg", url:"", abbreviations:"research 3"},
+    {category:"Technologies", image:"https://bit.bme.jhu.edu/img/menu/menu_msi.jpg", url:"", abbreviations:"research 4"},
+    {category:"Technologies", image:"https://bit.bme.jhu.edu/img/menu/menu_molecular.jpg", url:"", abbreviations:"research 5"},
+    {category:"Disease Diagnosis & Intervention Guidance", image:"https://bit.bme.jhu.edu/img/menu/menu_gi.jpg", url:"", abbreviations:"research 6"},
+    {category:"Disease Diagnosis & Intervention Guidance", image:"https://bit.bme.jhu.edu/img/menu/menu_airwayimaging.jpg", url:"", abbreviations:"research 7"},
+    {category:"Disease Diagnosis & Intervention Guidance", image:"https://bit.bme.jhu.edu/img/menu/menu_ptb.jpg", url:"", abbreviations:"research 8"},
+    {category:"Disease Diagnosis & Intervention Guidance", image:"https://bit.bme.jhu.edu/img/menu/menu_ptb.jpg", url:"", abbreviations:"research 9"},
+    {category:"Disease Diagnosis & Intervention Guidance", image:"https://bit.bme.jhu.edu/img/menu/menu_braintumorimaging.jpg", url:"", abbreviations:"research 10"},
+    {category:"Basic Science", image:"https://bit.bme.jhu.edu/img/menu/menu_brainfunctionalimaging.jpg", url:"", abbreviations:"research 11"}
+];
+
+export const newsList = [
+    {
+        date: new Date("2023-07-17"), 
+        content: "Hear about our research: Dr. Li will deliver an invited talk at iNOW2023 on ”Probing the Brain Using Low-coherence Light” in Wuerzburg, Germany.",
+        url: "https://www2.uni-wuerzburg.de/inow2023/program.php"
+    },
+    {
+        date: new Date("2023-06-27"),
+        content: "News and Views article on a novel technology for simultaneous optogenetic control and multi-color probing of neural circuits by Dr. Li published in Opto-Electronic Advances.",
+        url: "https://www.oejournal.org//article/doi/10.29026/oea.2023.230086"
+    },
+    {
+        date: new Date("2023-06-27"),
+        content: "Group member Nathan Wang won Goldwater Scholarship! Congratulations!",
+        url: "https://hub.jhu.edu/2023/06/27/four-win-goldwater-scholarships/"
+    },
+    {
+        date: new Date("2023-05-23"),
+        content: "Dr. Li gave a Grand Rounds talk at GMBC Head and Neck Center on \"Microscopic Biophotonics Imaging for Seeing the Unseen.\"",
+    },
+    {
+        date: new Date("2023-05-23"),
+        content: "Frontier Talk by Dr. Li at CYTO2023 in Montreal on \“Shedding Low Coherence Light on the Brain.\”",
+        url: "https://www.cytoconference.org/program.html"
+    },
+    {
+        date: new Date("2023-05-22"),
+        content: "Frontier Talk by Dr. Li at CYTO2023 in Montreal on \“Shedding Low Coherence Light on the Brain.\”",
+        url: "https://www.cytoconference.org/program.html"
+    },
+    {
+        date: new Date("2023-05-06"),
+        content: "Keynote Speech by Dr. Li on the Mid-Atlantic Research Day organized by Hopkins BMES."
+    },
+    {
+        date: new Date("2023-01-30"),
+        content: "Postdoc positions are available. Contact xingde@jhu.edu"
+    },
+    {
+        date: new Date("2023-01-28"),
+        content: "Hear about our research: Dr. Xingde Li will give a Keynote Talk at Photonics West on 2P fiberscopy for brain imaging in freely behaving rodents."
+    },
+    {
+        date: new Date("2023-01-04"),
+        content: "Our paper on direct visualization and quantitative imaging of small airways was chosen as a featured article by IEEE TBME (Jan issue 2023) !",
+        url: "https://www.embs.org/tbme/featured-articles/"
+    },
+    {
+        date: new Date("2023-01-01"),
+        content: "Congratulations to our former group member, Dr. Carmen Kut, MD, PhD on becoming a faculty member in Radiation Oncology Johns Hopkins School of Medicine!"
+    }
+];
