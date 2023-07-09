69384f9f29a0998569f366f17be5217c7095da04
quin <quineyugame@gmail.com>
2023-07-09 14:55:13 +0800

    mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,28 @@
 import React, {useContext} from 'react';
+import {tw} from '@twind/core';
 import {LabContext} from '../App';
 
 function Footer() {
     const labContext = useContext(LabContext);
     return (
-        <div className="flex-col px-4 pt-8 mx-auto  sm:max-w-xl md:max-w-full md:px-24 lg:px-8 bg-slate-800">
-            <div className="flex mx-auto">
-                <img className="w-80" src="https://www.jhu.edu/assets/themes/machado/assets/images/logos/university-logo-small-horizontal-white-156eae9527.svg" alt="Johns Hopkins University" />
-                <div className="grid gap-40 row-gap-6 mb-8 sm:grid-cols-3 lg:grid-cols-3  ml-8">
-                        <div className="flex items-center mt-4 text-gray-300">
+        <div className={tw`flex-col px-4 pt-8 mx-auto  sm:max-w-xl md:max-w-full md:px-24 lg:px-8 bg-slate-800`}>
+            <div className={tw`lg:flex mx-auto`}>
+                <img className={tw`w-80`} src="https://www.jhu.edu/assets/themes/machado/assets/images/logos/university-logo-small-horizontal-white-156eae9527.svg" alt="Johns Hopkins University" />
+                <div className={tw`grid gap-4 lg:gap-40 lg:row-gap-6 mb-8 grid-cols-1 lg:grid-cols-3  ml-8`}>
+                        <div className={tw`flex items-center mt-4 text-gray-300`}>
                             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-300">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
                             </svg>
-                            <div className="ml-4 text-md tracking-wide font-semibold">
+                            <div className={tw`ml-4 text-md tracking-wide font-semibold`}>
                                 720 Rutland Avenue,<br />Traylor 710 Baltimore,<br />MD 21205
                             </div>
                         </div>
-                        <div className="flex items-center text-gray-300">
+                        <div className={tw`flex items-center text-gray-300`}>
                             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-10 h-10 text-gray-300">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M5.25 6.75h13.5m-13.5 0v10.5h13.5V6.75m-13.5 0L12 12l6.75-5.25"></path>
                             </svg>
-                            <div className="ml-4 text-md tracking-wide font-semibold">
+                            <div className={tw`ml-4 text-md tracking-wide font-semibold`}>
                                 jhu.bme.bit@gmail.com
                             </div>
                         </div>
@@ -29,14 +30,14 @@ function Footer() {
                             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-300">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
                             </svg>
-                            <div className="ml-4 text-md tracking-wide font-semibold">
+                            <div className={tw`ml-4 text-md tracking-wide font-semibold`}>
                                 1 (410) 955-0075
                             </div>
                         </div>   
                 </div>
             </div>
-            <div className="flex flex-col content-end pt-5 pb-10 border-t lg:flex-row">
-                    <p className="text-sm text-gray-300">
+            <div className={tw`flex flex-col content-end pt-5 pb-10 border-t lg:flex-row`}>
+                    <p className={tw`text-sm md:text-sm text-gray-300`}>
                         © Copyright {new Date().getFullYear()} {labContext.name} Inc. All rights reserved.
                     </p>
             </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,15 +18,15 @@ function About() {
         <div>
             <div className={tw`flex flex-col items-center`}>
                 <img src="https://bit.bme.jhu.edu/img/hometop.png" className={tw`w-3/5`}></img>
-                <strong className={tw`mt-4 text-3xl tracking-wide text-gray-900`}>Welcome to the {labContext.name}</strong>
-                <p className={tw`indent-8 text-gray-600 text-2xl text-left whitespace-normal tracking-normal leading-relaxed w-3/4`}>
+                <strong className={tw`mt-4 text-xs md:text-sm lg:text-3xl tracking-wide text-gray-900`}>Welcome to the {labContext.name}</strong>
+                <p className={tw`indent-8 text-gray-600 text-xs md:text-sm lg:text-2xl text-left whitespace-normal tracking-normal leading-relaxed w-3/4`}>
                         Our research focuses on the development of cutting-edge and transformative biophotonics technologies with an ultimate goal of disease detection at early, manageable stages, monitoring 
                     therapeutic effects and treatment outcomes, and guiding interventions. Follow the links in <NavLink className="underline italic text-blue-600" to="/Research">Research</NavLink> to learn 
                     more or contact us for further information.
                 </p>
             </div>
 
-            <div className={tw`flex`}>
+            <div className={tw`lg:flex`}>
                 <div className={tw`flex ml-20 mt-10 w-1/2`}>
                     <Swiper
                         slidesPerView={1}
@@ -43,9 +43,9 @@ function About() {
                             researches.map((research,index) => {
                             return (
                                 <SwiperSlide key={index}>
-                                <div className={tw`grid grid-col-1 ml-40 w-7/12`}>
+                                <div className={tw`grid grid-col-1 md:ml-40 md:w-7/12`}>
                                     <img src={research.image}/>
-                                    <ReactMarkdown className={tw`indent-8 text-[18px]`}>Hear about our research: Dr. Li will deliver an invited talk at iNOW2023 on ”Probing the Brain Using Low-coherence Light” in Wuerzburg, Germany.</ReactMarkdown>
+                                    <ReactMarkdown className={tw`indent-8 text-xs md:text-sm lg:text-[18px]`}>Hear about our research: Dr. Li will deliver an invited talk at iNOW2023 on ”Probing the Brain Using Low-coherence Light” in Wuerzburg, Germany.</ReactMarkdown>
                                 </div>
                                 </SwiperSlide>
                             );
@@ -55,15 +55,15 @@ function About() {
                 </div>
                 
                 <div className={tw`container md:px-6 mt-2`}>
-                    <section className={tw`text-center md:text-left`}>
-                        <h2 className={tw`mb-2 text-xl font-bold`}>Latest News</h2>
+                    <section className={tw`text-left`}>
+                        <h2 className={tw`md:mb-2 text-xs md:text-sm lg:text-xl font-bold`}>Latest News</h2>
                         {
                             newsList.slice(0,4).map((news, index) => {
                                 return (
-                                    <div className={tw`mb-2 flex flex-wrap w-3/5`} key={index}>
+                                    <div className={tw`md:mb-2 flex flex-wrap md:w-3/5`} key={index}>
                                         <div className={tw`mb-2 mr-auto basis-auto px-3 md:mb-0`}>
                                             <small className={tw`font-bold`}>{news.date.toISOString().slice(0, 10)}    </small>
-                                            <span className={tw`text-neutral-500 dark:text-neutral-300`}>
+                                            <span className={tw`text-sx md:text-neutral-500`}>
                                                 {news.content}
                                             </span>
                                         </div>
@@ -71,7 +71,7 @@ function About() {
                                 );
                             })
                         }     
-                        <NavLink to="/news" className={tw`text-sm font-medium italic text-center text-blue-600 underline`}>
+                        <NavLink to="/news" className={tw`text-xm md:text-sm font-medium italic text-center text-blue-600 underline`}>
                             Read More
                         </NavLink>        
                     </section>
